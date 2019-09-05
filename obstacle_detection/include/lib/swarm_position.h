215c52561fdf6c915980a687517a488fd3a54511
Micha Sende <sende@lakeside-labs.com>
2019-09-05 12:58:11 +0200

    remove obstacle detection dependencies by including required libraries

---
+++ obstacle_detection/include/lib/swarm_position.h
@@ -0,0 +1,103 @@
+#ifndef SWARM_POSITION_H
+#define SWARM_POSITION_H
+
+#include <ros/ros.h>
+#include <cpswarm_msgs/ArrayOfVectors.h>
+#include <cpswarm_msgs/VectorStamped.h>
+#include "sector.h"
+
+using namespace std;
+using namespace ros;
+
+/**
+ * @brief A class that holds position information of the other CPSs in the swarm.
+ */
+class swarm_position
+{
+public:
+    /**
+     * @brief Constructor that initializes the private member variables.
+     */
+    swarm_position ();
+
+    /**
+     * @brief Check if the CPS is far enough from other CPSs ahead.
+     * @param heading The heading of the CPS.
+     * @return True if all other CPSs are either further away than avoidance distance or their bearing is outside of the heading angle of the CPS. False otherwise.
+     */
+    bool clear_ahead (double heading) const;
+
+    /**
+     * @brief Check if the CPS is dangerously close to other CPSs.
+     * @return The distance that the current CPS has to back off from the other CPS in order to reach a safe distance again. Returns 0.0 in case no CPS is closer than the critical distance.
+     */
+    double danger () const;
+
+    /**
+     * @brief Get the absolute sector occupied by other CPSs with added saftey bearing.
+     * @return The inflated occupied sector.
+     */
+    sector inflated_region () const;
+
+    /**
+     * @brief Get the absolute sector occupied by other CPSs.
+     * @return The occupied sector.
+     */
+    sector occupied_region () const;
+
+private:
+    /**
+     * Caclulate the bearing error.
+     * @param pose The pose (bearing, distance) to check.
+     * @return The bearing error.
+     */
+    double bearing_tolerance (cpswarm_msgs::VectorStamped pose) const;
+
+    /**
+     * @brief Callback function to receive the relative positions of the other CPSs.
+     * @param msg Distance and bearing of the other CPSs.
+     */
+    void swarm_pose_callback (const cpswarm_msgs::ArrayOfVectors::ConstPtr& msg);
+
+    /**
+     * @brief A node handle for the main ROS node.
+     */
+    NodeHandle nh;
+
+    /**
+     * @brief Subscriber for the relative positions of the other CPSs.
+     */
+    Subscriber swarm_pose_sub;
+
+    /**
+     * @brief The relative positions of the other swarm members (distance and bearing).
+     */
+    vector<vector<cpswarm_msgs::VectorStamped>> poses;
+
+    /**
+     * @brief The distance in meters which describes the uncertainty radius of the pose.
+     */
+    double pose_variation;
+
+    /**
+     * @brief The distance in meters to another CPSs below which they start their collision avoidance procedure.
+     */
+    double avoidance_dist;
+
+    /**
+     * @brief The distance in meters to another CPS below which they have to move apart first before the standard avoidance procedure.
+     */
+    double critical_dist;
+
+    /**
+     * @brief The angle in radian that must be clear on each side around the CPS's movement direction.
+     */
+    double clear_ang;
+
+    /**
+     * @brief The index of the latest position in the relative poses vectors.
+     */
+    unsigned int t;
+};
+
+#endif // SWARM_POSITION_H
---
+++ obstacle_detection/src/lib/swarm_position.cpp
@@ -0,0 +1,113 @@
+#include "lib/swarm_position.h"
+
+swarm_position::swarm_position ()
+{
+    // read parameters
+    int queue_size;
+    nh.param(this_node::getName() + "/queue_size", queue_size, 1);
+    nh.param(this_node::getName() + "/pose_variation", pose_variation, 0.1);
+    int hist;
+    nh.param(this_node::getName() + "/sample_hist", hist, 1);
+    nh.param(this_node::getName() + "/avoidance_dist", avoidance_dist, 2.0);
+    nh.param(this_node::getName() + "/critical_dist", critical_dist, 1.0);
+    nh.param(this_node::getName() + "/clear_ang", clear_ang, 0.785);
+
+    // initialize parameters
+    t = 0;
+
+    // initialize swarm position vectors
+    for (int i = 0; i < hist; ++i) {
+        vector<cpswarm_msgs::VectorStamped> vec;
+        poses.push_back(vec);
+    }
+
+    // publishers and subscribers
+    swarm_pose_sub = nh.subscribe("swarm_position_rel", queue_size, &swarm_position::swarm_pose_callback, this);
+}
+
+bool swarm_position::clear_ahead (double heading) const
+{
+    // the sector that must be clear of cpss
+    sector sec(heading-clear_ang, heading+clear_ang);
+
+    ROS_DEBUG("Sector to be clear [%.2f, %.2f]", sec.min(), sec.max());
+
+    // check current cps positions
+    for (auto pose : poses[t]) {
+        ROS_DEBUG("Checking pose [%.2f, %.2f]", pose.vector.magnitude, pose.vector.direction);
+        // cps is close enough to start avoidance procedure
+        if (pose.vector.magnitude < avoidance_dist) {
+            // cps is in critical sector
+            if (sec.contains(pose.vector.direction))
+                return false;
+        }
+    }
+
+    // no cps near by
+    return true;
+}
+
+double swarm_position::danger () const
+{
+    // check current cps positions
+    for (auto pose : poses[t]) {
+        // cps is dangerously close
+        if (pose.vector.magnitude < critical_dist) {
+            return critical_dist - pose.vector.magnitude ;
+        }
+    }
+
+    // no cps near by
+    return 0.0;
+}
+
+sector swarm_position::inflated_region () const
+{
+    // occupied sector
+    sector occ = occupied_region();
+
+    // inflate by clear_ang
+    if (occ.size() > 0)
+        occ.inflate(clear_ang);
+
+    // return inflated sector
+    return occ;
+}
+
+sector swarm_position::occupied_region () const
+{
+    // minimum and maximum bearing occupied by other cpss
+    double swarm_min = 0;
+    double swarm_max = 0;
+
+    // take latest poses
+    for (auto pose : poses[t]) {
+        ROS_DEBUG("Checking pose [%.2f, %.2f]", pose.vector.magnitude, pose.vector.direction);
+        if (pose.vector.magnitude <= avoidance_dist) {
+            double swarm_min_temp = pose.vector.direction - bearing_tolerance(pose);
+            double swarm_max_temp = pose.vector.direction + bearing_tolerance(pose);
+            if (swarm_min_temp < swarm_min || swarm_min == 0)
+                swarm_min = swarm_min_temp;
+            if (swarm_max_temp > swarm_max || swarm_max == 0)
+                swarm_max = swarm_max_temp;
+        }
+    }
+
+    // return sector
+    return sector(swarm_min, swarm_max);
+}
+
+double swarm_position::bearing_tolerance (cpswarm_msgs::VectorStamped pose) const
+{
+    return asin(pose_variation / pose.vector.magnitude);
+}
+
+void swarm_position::swarm_pose_callback (const cpswarm_msgs::ArrayOfVectors::ConstPtr& msg)
+{
+    // increase current index of positions vector
+    t++;
+    t %= poses.size();
+
+    // save positions
+    poses[t] = msg->vectors;
+}
