aedeb900b5c21a5b5e9b5d600b57c6d79bf11e13
Ángel Soriano <asoriano@robotnik.es>
2019-11-20 16:25:07 +0100

    call to goal action client just once

--- navigation_pos_controller/src/navigation_pos_controller_action_client.cpp
+++ navigation_pos_controller/src/navigation_pos_controller_action_client.cpp
@@ -4,6 +4,7 @@
 #include <std_msgs/Empty.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <move_base_msgs/MoveBaseAction.h>
+#include <std_srvs/Empty.h>
 
 using namespace std;
 using namespace ros;
@@ -73,6 +74,7 @@ void goal_callback(const geometry_msgs::PoseStamped::ConstPtr& msg)
 
     goal_valid = true;
     cancel_goal = false;
+    
 }
 
 /**
@@ -114,10 +116,12 @@ int main(int argc, char **argv)
     nh.param(this_node::getName() + "/goal_tolerance", goal_tolerance, 0.1);
     double goal_tolerance_a;
     nh.param(this_node::getName() + "/goal_tolerance_a", goal_tolerance_a, 0.1);
+    
+    //To clear costmap before navigation
+	ServiceClient clear_costmaps_client;
+	std_srvs::Empty clear_costmaps;
+	clear_costmaps_client = nh.serviceClient<std_srvs::Empty>("move_base/clear_costmaps");
 
-    // goal action message
-    move_base_msgs::MoveBaseGoal goal_msg;
-    goal_msg.target_pose.header.frame_id = "map";
 
     // wait for valid pose and goal
     pose_valid = false;
@@ -129,7 +133,6 @@ int main(int argc, char **argv)
 
     // move base action client in separate thread
     MoveBaseClient ac("move_base", true);
-    Publisher publisher = nh.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", queue_size);
 
     // wait for valid position
     while (ok() && pose_valid == false) {
@@ -139,7 +142,9 @@ int main(int argc, char **argv)
     }
 
     // wait for move base action server
-    ac.waitForServer();
+    while(!ac.waitForServer(ros::Duration(5.0))){
+		ROS_INFO("Waiting for the move_base action server to come up");
+	}
 
     // goal subscribers
     Subscriber goal_sub = nh.subscribe("pos_controller/goal_position", queue_size, goal_callback);
@@ -152,61 +157,39 @@ int main(int argc, char **argv)
         // get position and goal update
         spinOnce();
 
-        // only move if goal is far enough from current pose
-        if (goal_valid && (hypot(goal.position.x - pose.position.x, goal.position.y - pose.position.y) >= goal_tolerance || abs(remainder(get_yaw(pose) - get_yaw(goal), 2*M_PI)) >= goal_tolerance_a))) {
-            ROS_DEBUG("POS_CTRL - Move to (%.2f,%.2f,%.2f).", goal.position.x, goal.position.y, get_yaw(goal));
-
-            // create goal message
-            goal_msg.target_pose.header.seq++;
-            goal_msg.target_pose.header.stamp = Time::now();
-            goal_msg.target_pose.pose = goal;
-
-            // shift goal according to origin
-            goal_msg.target_pose.pose.position.x -= origin.position.x;
-            goal_msg.target_pose.pose.position.y -= origin.position.y;
-
-            // send goal to ugv
-            ac.sendGoalAndWait(goal_msg);
-
-            // wait while goal is actively pursued
-            while (ac.getState() == actionlib::SimpleClientGoalState::PENDING || ac.getState() == actionlib::SimpleClientGoalState::ACTIVE) {
-                // check state
-                if (ac.getState() == actionlib::SimpleClientGoalState::PENDING)
-                    ROS_DEBUG_THROTTLE(10, "POS_CTRL - Goal pending...");
-                else
-                    ROS_DEBUG_THROTTLE(10, "POS_CTRL - Goal active...");
-
-                spinOnce();
-
-                // stop moving
-                if (cancel_goal == true || goal_valid == false) {
-                    ac.cancelGoal();
-                    ROS_DEBUG("Goal canceled");
-                    cancel_goal = false;
-                    break;
-                }
-
-                rate.sleep();
-            }
-
-            // action server finished successfully
-            if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
-                ROS_DEBUG("POS_CTRL - Reached goal (%.2f,%.2f,%.2f).", goal.position.x, goal.position.y, get_yaw(goal));
-            }
-
-            // action server failed
-            else {
-                ROS_ERROR("POS_CTRL - Failed to reach goal (%.2f,%.2f,%.2f), goal %s.", goal.position.x, goal.position.y, get_yaw(goal), ac.getState().toString().c_str());
-            }
-        }
-
-        // stop moving
-        else if (cancel_goal){
-            ac.cancelAllGoals();
-            ROS_DEBUG("Goal canceled");
-            cancel_goal = false;
-            goal_valid = false;
-        }
+		//if there is a new goal
+		if(goal_valid){
+			
+			// goal action message
+			move_base_msgs::MoveBaseGoal goal_msg;
+			goal_msg.target_pose.header.frame_id = "map";
+			
+			// create goal message
+			goal_msg.target_pose.header.seq++;
+			goal_msg.target_pose.header.stamp = Time::now();
+			goal_msg.target_pose.pose = goal;
+			
+			// shift goal according to origin
+			goal_msg.target_pose.pose.position.x -= origin.position.x;
+			goal_msg.target_pose.pose.position.y -= origin.position.y;
+			
+			//clear costmaps
+			if(clear_costmaps_client.call(clear_costmaps)){
+				ROS_INFO("clearing costmaps");
+			}else{
+				ROS_ERROR("error clearing costmaps");
+			}
+			
+			// send goal to ugv
+			ac.sendGoalAndWait(goal_msg);
+			
+			if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+				ROS_INFO("Goal reached!");
+			else
+				ROS_INFO("The base failed to move for some reason");
+				
+			goal_valid = false;
+		}       
 
         // sleep rest of cycle
         rate.sleep();
