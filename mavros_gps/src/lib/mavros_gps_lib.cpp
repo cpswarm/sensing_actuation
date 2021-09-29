72028fecd511a7a6a9230845b5b308d7096b909d
Micha Sende <sende@lakeside-labs.com>
2021-09-29 13:38:20 +0200

    fix 302e86d (mavros issue 1414) to work also with ardupilot

--- mavros_gps/src/lib/mavros_gps_lib.cpp
+++ mavros_gps/src/lib/mavros_gps_lib.cpp
@@ -1,6 +1,6 @@
 #include "lib/mavros_gps_lib.h"
 
-mavros_gps_lib::mavros_gps_lib ()
+mavros_gps_lib::mavros_gps_lib () : geoid("egm96-5", "", true, true)
 {
     // read parameters
     double loop_rate;
@@ -13,14 +13,6 @@ mavros_gps_lib::mavros_gps_lib ()
     pose_sub = nh.subscribe("mavros/global_position/global", queue_size, &mavros_gps_lib::pose_callback, this);
     while (ok() && origin.latitude == 0) {
         ROS_DEBUG_THROTTLE(10, "Waiting for valid GPS...");
-        rate.sleep();
-        spinOnce();
-    }
-
-    // init altitude difference between msl and wgs-84
-    altitude_sub = nh.subscribe("mavros/altitude", queue_size, &mavros_gps_lib::altitude_callback, this);
-    while (ok() && altitude_fix == 0) {
-        ROS_DEBUG_ONCE("Waiting for valid altitude...");
         rate.sleep();
         spinOnce();
     }
@@ -70,12 +62,10 @@ bool mavros_gps_lib::pose_to_fix (cpswarm_msgs::PoseToFix::Request &req, cpswarm
     double dist = hypot(req.pose.pose.position.x, req.pose.pose.position.y);
     double head = atan2(req.pose.pose.position.y, req.pose.pose.position.x);
     res.fix = goal(origin, dist, head);
+    res.fix.altitude = req.pose.pose.position.z + origin.altitude;
 
     // copy header
     res.fix.header = req.pose.header;
-
-    // copy altitude
-    res.fix.altitude = req.pose.pose.position.z;
 
     return true;
 }
@@ -88,9 +78,7 @@ bool mavros_gps_lib::pose_to_geo (cpswarm_msgs::PoseToGeo::Request &req, cpswarm
     sensor_msgs::NavSatFix fix = goal(origin, dist, head);
     res.geo.pose.position.latitude = fix.latitude;
     res.geo.pose.position.longitude = fix.longitude;
-
-    // convert altitude to wgs-84
-    res.geo.pose.position.altitude = req.pose.pose.position.z - altitude_fix;
+    res.geo.pose.position.altitude = req.pose.pose.position.z + altitude(fix);
 
     // copy header
     res.geo.header = req.pose.header;
@@ -124,6 +112,11 @@ bool mavros_gps_lib::target_to_fix (mavros_gps::TargetToFix::Request &req, mavro
 {
     res.fix = target_to_fix(req.target);
     return true;
+}
+
+double mavros_gps_lib::altitude (sensor_msgs::NavSatFix gps)
+{
+    return gps.altitude + GeographicLib::Geoid::ELLIPSOIDTOGEOID * geoid(gps.latitude, gps.longitude);
 }
 
 double mavros_gps_lib::dist (sensor_msgs::NavSatFix start, sensor_msgs::NavSatFix goal) const
@@ -256,15 +249,4 @@ void mavros_gps_lib::pose_callback (const sensor_msgs::NavSatFix::ConstPtr& msg)
         // unsubscribe
         pose_sub.shutdown();
     }
-}
-
-void mavros_gps_lib::altitude_callback (const mavros_msgs::Altitude::ConstPtr& msg)
-{
-    // store altitude in class variable
-    altitude_fix = origin.altitude - msg->amsl;
-
-	ROS_DEBUG("Got altitude AMSL %.2f", msg->amsl);
-
-    // unsubscribe
-    altitude_sub.shutdown();
 }
